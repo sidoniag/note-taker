a2b90c0f773a71c6bc87bd3686d7bb3ed2003b8a
idonia <sidoniag@gmail.com>
2020-08-02 18:04:30 -0500

    add get .html files

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+const notes = require('.db/db.json');
+
+app.get('/db/db', (req, res) => {
+    res.json(notes);
+});
+
+// connect index.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '.public/index.html'));
+});
+
+// connect notes.html file
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.listen(PORT, () => {
+    console.log('API server now on port ${PORT}!');
+})
