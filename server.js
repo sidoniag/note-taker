0a9fd4b8c0c3e504b02290ac826384e510be81f6
idonia <sidoniag@gmail.com>
2020-08-02 18:27:07 -0500

    verified index.html

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const notes = require('.db/db.json');
+const notes = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
@@ -12,18 +12,18 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
-app.get('/db/db', (req, res) => {
-    res.json(notes);
-});
+// app.get('/db/db', (req, res) => {
+//     res.json(notes);
+// });
 
 // connect index.html file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '.public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 // connect notes.html file
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 app.listen(PORT, () => {
