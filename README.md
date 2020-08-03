32b5e0c96387847a58729e78465e7ffb92c3d1ff
idonia <sidoniag@gmail.com>
2020-08-02 20:38:30 -0500

    add functions

--- README.md
+++ README.md
@@ -12,7 +12,10 @@
 An application that allows the user to add, save, and delete notes. Using the app is easy - to add a note simply input the note and click the save button. The note will be stored in the column on the left. To see details of the note, click on it to expand details displayed on the righ hand side. The delete button will remove the note from the site and server. 
 
 ## Technologies
-Run npm install to install necessary packages
+HTML
+CSS
+Express.js
+
 
 ## Screenshots
 
--- server.js
+++ server.js
@@ -12,9 +12,51 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
-// app.get('/db/db', (req, res) => {
-//     res.json(notes);
-// });
+// functions
+function filterByQuery(query, notesArray) {
+    let filteredResults = notesArray;
+    if (query.title) {
+        filteredResults = filteredResults.filter(note => note.title === query.title);
+    }
+    if (query.text) {
+        filteredResults = filteredResults.filter(note => note.text === query.text);
+    }
+    return filteredResults;
+}
+
+function findById(id, notesArray) {
+    const result = notesArray.filter(notes => notes.id === id)[0];
+    return result;
+}
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    notesArray.push(newNote);
+    fs.writeFileSync(path.join(__dirname, './db/db.json'),
+    JSON.stringify(notes, null, 2)
+    );
+    
+    return newNote;
+}
+
+function validateNote(note) {
+    if (!notes.title || typeof notes.title !== 'string') {
+        return false;
+    }
+    if (!notes.text || typeof notes.text !== 'string') {
+        return false;
+    }
+    return true;
+}
+
+// route to notes
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    if (req.query) {
+        results = filterByQuery(req.query, results)
+    }
+    res.json(results);
+});
 
 // connect index.html file
 app.get('/', (req, res) => {
